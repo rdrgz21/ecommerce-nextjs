766701c2ba363f42717ec5826a6dd07fa573c2b5
thomas.rodriguez <thomas.rodriguez@sharpgaming.com>
2022-08-04 16:34:42 +0100

    Added home and individual product pages

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Tom's Headphone Store</Link>
+      </p>
+      <button type='button' className='cart-icon' onClick=""></button>
+      <AiOutlineShopping />
+      <span className='cart-item-qty'>1</span>
+    </div>
+  );
+}
+
+export default Navbar
